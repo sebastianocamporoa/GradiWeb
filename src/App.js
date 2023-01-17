20a7f887a805dd3de96eb4ce24a6afff2cf79bc6
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-01-17 08:46:04 -0500

    instalación de framer-motion y nodemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { motion } from "framer-motion";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <motion.h1
+        initial={{ color: "#000000" }}
+        transition={{ duration: 3 }}
+        animate={{ y: 50, color: "#FFFF00" }}
+      >
+        Hello world
+      </motion.h1>
     </div>
   );
 }
